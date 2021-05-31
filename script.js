9f7bfbc2dc9dc4b0f66f7924183ebff763166c67
vmaffioli <vinimaffioli@gmail.com>
2021-05-31 15:19:35 -0300

    fixed syntax error script.js ln37

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function update (event){
 
 function iniciarJogo(){
 
-if(snake[0].x > 15 * box && direction == "rigth") snake[0].x = 0;
+if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
 if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
 if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
 if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
